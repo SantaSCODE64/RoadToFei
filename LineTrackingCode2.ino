22495b1a951cc32cc3abf31246c2358e874d923a
SantaSCODE64 <119875196+SantaSCODE64@users.noreply.github.com>
2023-03-09 10:40:06 +0100

    Robot Code Version 4.0

--- LineTrackingCode2.ino
+++ LineTrackingCode2.ino
@@ -153,41 +153,60 @@ void loop() {
   // Mozes menit rychlosti ja som ich tam dal len tak odhadom
   if (ir[0] == 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0) {
       lastSide = 'm';
-      goForward(60);
+      while(ir[0] == 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0){
+          goForward(60);
+      } 
   }
   else if (ir[0] == 1 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0){
-      lastSide = 'l'; 
-      // tato funkcia dostava pohon do praveho kolesa dopredu a do laveho kolesa dozadu takze sa uplne otoci, ale ked to nebude fungovat tak to vymen za klasicke turnLeft
-      turnFullLeft(60);
+      lastSide = 'l';
+      while (ir[0] == 1 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0){
+        // tato funkcia dostava pohon do praveho kolesa dopredu a do laveho kolesa dozadu takze sa uplne otoci, ale ked to nebude fungovat tak to vymen za klasicke turnLeft
+          turnFullLeft(60);
+      }
+      
   }
   else if (ir[0] == 1 && ir[1] == 1 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0){
-      lastSide = 'l';
-      turnLeft(60);
+    lastSide = 'l';
+    while(ir[0] == 1 && ir[1] == 1 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0){
+        turnLeft(60);
+    }   
   }
   else if (ir[0] == 0 && ir[1] == 1 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0){
       lastSide = 'l';
-      turnLeft(60);
+      while(ir[0] == 0 && ir[1] == 1 && ir[2] == 0 && ir[3] == 0 && ir[4] == 0){
+          turnLeft(60);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0){
       lastSide = 'l';
-      turnLeft(40);
+      while(ir[0] == 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0){
+          turnLeft(40);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0){
       lastSide = 'r';
-      turnRight(40);
+      while(ir[0] == 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0){
+          turnRight(40);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 1 && ir[4] == 0){
       lastSide = 'r';
-      turnRight(60);
+      while(ir[0] == 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 1 && ir[4] == 0){
+          turnRight(60);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 1 && ir[4] == 1){
       lastSide = 'r';
-      turnRight(60);
+      while(ir[0] == 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 1 && ir[4] == 1){
+          turnRight(60);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 1){
       lastSide = 'r';
-      // tato funkcia dostava pohon do laveho kolesa dopredu a do praveho kolesa dozadu takze sa uplne otoci, ale ked to nebude fungovat tak to vymen za klasicke turnRight
-      turnFullRight(60);
+      while(ir[0] == 0 && ir[1] == 0 && ir[2] == 0 && ir[3] == 0 && ir[4] == 1){
+          // tato funkcia dostava pohon do laveho kolesa dopredu a do praveho kolesa dozadu takze sa uplne otoci, ale ked to nebude fungovat tak to vymen za klasicke turnRight
+          turnFullRight(60);
+      }
   }
   else if (ir[0] == 1 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 1){
     while(ir[0] == 1 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 1){
@@ -196,23 +215,33 @@ void loop() {
   }
   else if (ir[0] == 1 && ir[1] == 1 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0){
       lastSide = 'l';
-      turnLeft(80);
+      while(ir[0] == 1 && ir[1] == 1 && ir[2] == 1 && ir[3] == 0 && ir[4] == 0){
+          turnLeft(80);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 1 && ir[4] == 1){
       lastSide = 'r';
-      turnRight(80);
+      while(ir[0] == 0 && ir[1] == 0 && ir[2] == 1 && ir[3] == 1 && ir[4] == 1){
+          turnRight(80);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0){
       lastSide = 'm';
-      goForward(60);
+      while(ir[0] == 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0){
+          goForward(60);
+      }
   }
   else if (ir[0] == 1 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0){
       lastSide = 'l';
-      turnFullLeft(80);
+      while(ir[0] == 1 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 0){
+          turnFullLeft(80);
+      }
   }
   else if (ir[0] == 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 1){
       lastSide = 'r';
-      turnFullRight(80);
+      while(ir[0] == 0 && ir[1] == 1 && ir[2] == 1 && ir[3] == 1 && ir[4] == 1){
+          turnFullRight(80);
+      }
   }
   else if(sum_of_ir == 0){
     //Prvy krat to skusaj s touto funkciou goForward, ale vytvoril som funkciu findLine ktoru v tejto podmienke mozes otestovat
